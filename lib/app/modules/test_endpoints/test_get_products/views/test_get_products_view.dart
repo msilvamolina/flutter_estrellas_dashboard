2a1b066c39863449b810c4c36f8653ed3f8a8e81
Martín Silva Molina <msilvamolina@gmail.com>
2024-09-29 19:44:29 +0200

    .

--- lib/app/modules/test_endpoints/test_get_products/views/test_get_products_view.dart
+++ lib/app/modules/test_endpoints/test_get_products/views/test_get_products_view.dart
@@ -1,8 +1,10 @@
+import 'package:estrellas_dashboard/app/themes/styles/typography.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
 import '../../../../app/layouts/main_layout/main_layout.dart';
+import '../../../../components/widgets/button.dart';
 import '../../../../routes/app_pages.dart';
 import '../controllers/test_get_products_controller.dart';
 
@@ -14,8 +16,21 @@ class TestGetProductsView extends GetView<TestGetProductsController> {
       mainCurrentRoute: Routes.TEST_ENDPOINTS,
       currentRoute: Routes.TEST_GET_PRODUCTS,
       appBarTitle: 'Test Get Products',
-      child: Center(
-        child: Text('Test Get Products'),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Button(
+            label: 'Get Products',
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          Text(
+            'Result',
+            style: TypographyStyle.h3Mobile,
+          ),
+        ],
       ),
     );
   }
