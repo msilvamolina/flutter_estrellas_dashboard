aa9d3101841a8e521ea91a0c24680309bf920834
Martín Silva Molina <msilvamolina@gmail.com>
2024-10-20 04:49:24 +0200

    crear producto

--- lib/app/modules/products/create_product/controllers/create_product_controller.dart
+++ lib/app/modules/products/create_product/controllers/create_product_controller.dart
@@ -1,65 +1,137 @@
+import 'package:dartz/dartz.dart';
+import 'package:estrellas_dashboard/app/data/models/provider/provider/provider_model.dart';
+import 'package:estrellas_dashboard/app/data/providers/repositories/providers/providers_repository.dart';
 import 'package:get/get.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:reactive_forms/reactive_forms.dart';
 
-import '../../../../data/providers/repositories/products/products_repository.dart';
+import '../../../../app/controllers/main_controller.dart';
+import '../../../../utils/utils_image.dart';
+
+enum Fields {
+  name('name'),
+  email('email'),
+  surname('surname'),
+  phone('phone'),
+  document('document'),
+  porcentage('porcentage'),
+  ;
+
+  const Fields(this.text);
+  final String text;
+}
 
 class CreateProductController extends GetxController {
-  final ProductsRepository _repository = ProductsRepository();
+  final ProvidersRepository _repository = ProvidersRepository();
+  final MainController _mainController = Get.find<MainController>();
 
-  bool _isLoading = false;
-  bool get isLoading => _isLoading;
+  String? _imagePath;
+  String? get imagePath => _imagePath;
 
-  String? _responseError;
-  String? get responseError => _responseError;
+  FormGroup buildForm() => fb.group(<String, Object>{
+        Fields.name.name: FormControl<String>(
+          validators: [
+            Validators.required,
+            Validators.minLength(4),
+          ],
+        ),
+        Fields.surname.name: FormControl<String>(
+          validators: [
+            Validators.required,
+            Validators.minLength(4),
+          ],
+        ),
+        Fields.email.name: FormControl<String>(
+          validators: [
+            Validators.required,
+            Validators.email,
+            Validators.minLength(4),
+          ],
+        ),
+        Fields.phone.name: FormControl<String>(
+          validators: [
+            Validators.required,
+            Validators.number(),
+            Validators.minLength(4),
+          ],
+        ),
+        Fields.document.name: FormControl<String>(
+          validators: [
+            Validators.required,
+            Validators.number(),
+            Validators.minLength(4),
+          ],
+        ),
+        Fields.porcentage.name: FormControl<String>(
+          validators: [
+            Validators.required,
+            Validators.number(),
+            Validators.minLength(1),
+          ],
+        ),
+      });
 
-  Future<void> createProduct() async {
-    await _repository.createProduct();
+  @override
+  Future<void> onInit() async {
+    super.onInit();
   }
 
-  Future<void> createProvider() async {
-    await _repository.createProvider();
+  Future<void> pickImage() async {
+    _imagePath = await UtilsImage.pickImage();
+    update(['view']);
   }
 
-  Future<void> getProviders() async {
-    await _repository.getProviders();
+  Future<void> saveInFirebase({
+    required ProviderModel provider,
+  }) async {
+    _mainController.setDropiDialog(false);
+
+    _mainController.setDropiMessage('saveInFirebase');
+    Either<String, Unit> response = await _repository.saveProviderInFirebase(
+      provider: provider,
+    );
+    response.fold((failure) {
+      _mainController.setDropiDialogError(true, failure);
+    }, (_) async {
+      _mainController.setDropiMessage('Success!');
+      await Future.delayed(const Duration(seconds: 1), () {
+        Get.back();
+        Get.back(result: provider);
+      });
+    });
   }
 
-  Future<void> getNewProducts() async {
-    await _repository.getNewProducts();
-  }
+  Future<void> sendForm(Map<String, Object?> data) async {
+    if (_imagePath == null) {
+      Get.snackbar('Error', "Tienes que elegir una imagen");
+      return;
+    }
+    String name = data[Fields.name.name].toString();
+    String surname = data[Fields.surname.name].toString();
+    String email = data[Fields.email.name].toString();
+    String phone = data[Fields.phone.name].toString();
+    String document = data[Fields.document.name].toString();
+    String porcentage = data[Fields.porcentage.name].toString();
 
-  Future<void> createOrder() async {
-    await _repository.createOrder();
-  }
+    _mainController.setDropiDialog(true);
+    _mainController.showDropiLoader();
+    _mainController.setDropiMessage('Iniciando conexión');
 
-  Future<void> createMultipleOrder() async {
-    await _repository.createMultipleOrder();
-  }
-
-  Future<void> getOrders() async {
-    await _repository.getOrders();
-  }
-
-  Future<void> createWarehouse() async {
-    await _repository.createWarehouse();
-  }
-
-  Future<void> getOrderById() async {
-    await _repository.getOrderById('2859363');
-  }
-
-  Future<void> getMultipleOrderById() async {
-    await _repository.getOrderById('2859364');
-  }
-
-  Future<void> getDepartments() async {
-    await _repository.getDepartments();
-  }
-
-  Future<void> getCities() async {
-    await _repository.getCities('49');
-  }
-
-  Future<void> getCategories() async {
-    await _repository.getCategories();
+    Either<String, ProviderModel> response = await _repository.createProvider(
+      avatarURL: _imagePath!,
+      name: name,
+      surname: surname,
+      email: email,
+      phone: phone,
+      document: document,
+      porcentage: porcentage,
+    );
+    response.fold((failure) {
+      _mainController.setDropiDialogError(true, failure);
+    }, (provider) async {
+      _mainController.setDropiMessage('Success!');
+      saveInFirebase(provider: provider);
+    });
   }
 }
--- lib/app/modules/products/create_product/views/create_product_view.dart
+++ lib/app/modules/products/create_product/views/create_product_view.dart
@@ -1,106 +1,162 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:reactive_forms/reactive_forms.dart';
 
 import '../../../../app/layouts/main_layout/main_layout.dart';
 import '../../../../components/appbars/appbar_title_with_back.dart';
 import '../../../../components/widgets/loadingButton.dart';
 import '../../../../routes/app_pages.dart';
+import '../../../../themes/input_decoration.dart';
+import '../../../../themes/styles/typography.dart';
 import '../controllers/create_product_controller.dart';
 
 class CreateProductView extends GetView<CreateProductController> {
   const CreateProductView({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MainLayout(
-      showMenu: false,
-      currentRoute: Routes.CREATE_PRODUCT,
-      appBarTitle: 'Crear producto',
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(height: 16),
-            LoadingButton(
-              label: 'Crear',
-              isLoading: controller.isLoading,
-              onPressed: controller.createProduct,
+    Color primary = Theme.of(context).colorScheme.primary;
+
+    return ReactiveFormBuilder(
+        form: controller.buildForm,
+        builder: (context, form, child) {
+          return MainLayout(
+            showMenu: false,
+            currentRoute: Routes.CREATE_PROVIDER,
+            appBarTitle: 'Crear producto',
+            child: GetBuilder<CreateProductController>(
+              id: 'view',
+              builder: (_) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Card(
+                      child: InkWell(
+                        onTap: controller.pickImage,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Image.asset(
+                                controller.imagePath ??
+                                    'assets/images/picture.png',
+                                width: 80,
+                              ),
+                              const SizedBox(width: 12),
+                              // Elimina el Padding alrededor del Expanded
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Avatar Url',
+                                      style: TypographyStyle.bodyBlackLarge
+                                          .copyWith(color: primary),
+                                    ),
+                                    const SizedBox(height: 4),
+                                    Text(
+                                      controller.imagePath ??
+                                          '(Selecciona una foto)',
+                                      style: TypographyStyle.bodyRegularSmall,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Icon(
+                                Icons.chevron_right_rounded,
+                                color: primary,
+                                size: 48,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+
+                    ReactiveTextField(
+                      formControlName: Fields.name.name,
+                      keyboardType: TextInputType.text,
+                      decoration: CustomInputDecoration.inputDecoration(
+                        text: "Name",
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    ReactiveTextField(
+                      formControlName: Fields.surname.name,
+                      keyboardType: TextInputType.text,
+                      decoration: CustomInputDecoration.inputDecoration(
+                        text: "Surname",
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    ReactiveTextField(
+                      formControlName: Fields.email.name,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: CustomInputDecoration.inputDecoration(
+                        text: "E-mail",
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    ReactiveTextField(
+                      formControlName: Fields.phone.name,
+                      keyboardType: TextInputType.phone,
+                      decoration: CustomInputDecoration.inputDecoration(
+                        text: "Phone",
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    ReactiveTextField(
+                      formControlName: Fields.document.name,
+                      keyboardType: TextInputType.number,
+                      decoration: CustomInputDecoration.inputDecoration(
+                        text: "Document",
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    ReactiveTextField(
+                      formControlName: Fields.porcentage.name,
+                      keyboardType: TextInputType.number,
+                      decoration: CustomInputDecoration.inputDecoration(
+                        text: "Porcentage",
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    // Obx(
+                    //   () => DropDown(
+                    //     error: controller.productsError,
+                    //     selectedValue: controller.productSelected,
+                    //     values: controller.listProducts
+                    //         .map(
+                    //           (ProductFirebaseModel value) => OptionDropDown(
+                    //             text: value.name,
+                    //             value: value.id,
+                    //           ),
+                    //         )
+                    //         .toList(),
+                    //     onChanged: controller.onProductSelected,
+                    //   ),
+                    // ),
+                    const SizedBox(height: 26),
+                    ReactiveFormConsumer(
+                      builder: (context, form, child) => LoadingButton(
+                        label: 'Crear',
+                        // isLoading: controller.loading,
+                        isLoading: false,
+                        onPressed: (form.valid)
+                            ? () => controller.sendForm(form.value)
+                            : null,
+                      ),
+                    ),
+                  ],
+                );
+              },
             ),
-            const SizedBox(height: 16),
-            LoadingButton(
-              label: 'createProvider',
-              isLoading: controller.isLoading,
-              onPressed: controller.createProvider,
-            ),
-            const SizedBox(height: 16),
-            LoadingButton(
-              label: 'getProviders',
-              isLoading: controller.isLoading,
-              onPressed: controller.getProviders,
-            ),
-            const SizedBox(height: 16),
-            LoadingButton(
-              label: 'getNewProducts',
-              isLoading: controller.isLoading,
-              onPressed: controller.getNewProducts,
-            ),
-            const SizedBox(height: 16),
-            LoadingButton(
-              label: 'createOrder',
-              isLoading: controller.isLoading,
-              onPressed: controller.createOrder,
-            ),
-            const SizedBox(height: 16),
-            LoadingButton(
-              label: 'createMultipleOrder',
-              isLoading: controller.isLoading,
-              onPressed: controller.createMultipleOrder,
-            ),
-            const SizedBox(height: 16),
-            LoadingButton(
-              label: 'get order OrderById',
-              isLoading: controller.isLoading,
-              onPressed: controller.getOrderById,
-            ),
-            const SizedBox(height: 16),
-            LoadingButton(
-              label: 'get Multiple OrderById',
-              isLoading: controller.isLoading,
-              onPressed: controller.getMultipleOrderById,
-            ),
-            const SizedBox(height: 16),
-            LoadingButton(
-              label: 'getDepartments',
-              isLoading: controller.isLoading,
-              onPressed: controller.getDepartments,
-            ),
-            const SizedBox(height: 16),
-            LoadingButton(
-              label: 'getCities',
-              isLoading: controller.isLoading,
-              onPressed: controller.getCities,
-            ),
-            const SizedBox(height: 16),
-            LoadingButton(
-              label: 'getCategories',
-              isLoading: controller.isLoading,
-              onPressed: controller.getCategories,
-            ),
-            const SizedBox(height: 16),
-            LoadingButton(
-              label: 'getOrders',
-              isLoading: controller.isLoading,
-              onPressed: controller.getOrders,
-            ),
-            const SizedBox(height: 16),
-            LoadingButton(
-              label: 'createWarehouse',
-              isLoading: controller.isLoading,
-              onPressed: controller.createWarehouse,
-            ),
-            const SizedBox(height: 80),
-          ],
-        ),
-      ),
-    );
+          );
+        });
   }
 }
