679d7092625bf98363a87e3ba16f27be7468772d
Martín Silva Molina <msilvamolina@gmail.com>
2024-11-11 23:25:04 +0100

    new copy departments

--- lib/app/modules/admin/copy_departments/views/copy_departments_view.dart
+++ lib/app/modules/admin/copy_departments/views/copy_departments_view.dart
@@ -2,22 +2,27 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../../app/layouts/main_layout/main_layout.dart';
+import '../../../../routes/app_pages.dart';
 import '../controllers/copy_departments_controller.dart';
 
 class CopyDepartmentsView extends GetView<CopyDepartmentsController> {
   const CopyDepartmentsView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('CopyDepartmentsView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'CopyDepartmentsView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+    return MainLayout(
+      maxWidth: 900,
+      mainCurrentRoute: Routes.ADMIN,
+      currentRoute: Routes.COPY_DEPARTMENTS,
+      appBarTitle: 'Copy departments',
+      child: GetBuilder<CopyDepartmentsController>(
+        id: 'view',
+        builder: (_) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [],
+          );
+        },
       ),
     );
   }
