760161a8550f78e1442ed002509ab6dfacfd1d1a
Martín Silva Molina <msilvamolina@gmail.com>
2025-01-04 11:59:00 +0100

    ya tenemos la pantalla de combinaciones

--- lib/app/modules/new_variations/widgets/variations_card.dart
+++ lib/app/modules/new_variations/widgets/variations_card.dart
@@ -1,4 +1,5 @@
 import 'package:estrellas_dashboard/app/themes/styles/typography.dart';
+import 'package:estrellas_dashboard/app/utils/money_amount.dart';
 import 'package:flutter/material.dart';
 import '../../../data/models/product_variant/product_variant_model.dart';
 import '../../../data/models/variant_variant/variant_variant.dart';
@@ -24,19 +25,29 @@ class VariationsCard extends StatelessWidget {
           children: [
             Row(
               children: [
-                Wrap(
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    ...variation.values.asMap().entries.map(
-                      (entry) {
-                        final index = entry.key;
-                        final value = entry.value;
-                        final isLast = index == variation.values.length - 1;
-                        String name = value['value'];
-                        return Text(
-                          isLast ? name : '$name - ',
-                          style: TypographyStyle.bodyBlackLarge,
-                        );
-                      },
+                    Wrap(
+                      children: [
+                        ...variation.values.asMap().entries.map(
+                          (entry) {
+                            final index = entry.key;
+                            final value = entry.value;
+                            final isLast = index == variation.values.length - 1;
+                            String name = value['value'];
+                            return SelectableText(
+                              isLast ? name : '$name - ',
+                              style: TypographyStyle.bodyBlackLarge,
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                    SelectableText(
+                      variation.id,
+                      style: TypographyStyle.bodyRegularMedium,
                     ),
                   ],
                 ),
@@ -47,6 +58,7 @@ class VariationsCard extends StatelessWidget {
                 ),
               ],
             ),
+            SizedBox(height: 8),
             Row(
               children: [
                 ...variation.values.map(
@@ -63,9 +75,59 @@ class VariationsCard extends StatelessWidget {
                 )
               ],
             ),
+            SizedBox(height: 16),
+            Wrap(
+              children: [
+                priceCard(
+                  context: context,
+                  title: 'Precio',
+                  subtitle:
+                      MoneyAmount.convertMoneyString(variation.sale_price),
+                ),
+                priceCard(
+                  context: context,
+                  title: 'Precio Sugerido',
+                  subtitle:
+                      MoneyAmount.convertMoneyString(variation.suggested_price),
+                ),
+                priceCard(
+                  context: context,
+                  title: 'Puntos',
+                  subtitle: variation.points.toString(),
+                ),
+                priceCard(
+                  context: context,
+                  title: 'Stock',
+                  subtitle: variation.stock.toString(),
+                ),
+              ],
+            ),
           ],
         ),
       ),
     );
   }
+
+  Widget priceCard({
+    required BuildContext context,
+    required String title,
+    required String subtitle,
+  }) =>
+      Padding(
+        padding: const EdgeInsets.only(right: 16, bottom: 8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SelectableText(
+              title,
+              style: TypographyStyle.bodyBlackSmall
+                  .copyWith(color: Theme.of(context).colorScheme.primary),
+            ),
+            SelectableText(
+              subtitle,
+              style: TypographyStyle.bodyRegularMedium,
+            ),
+          ],
+        ),
+      );
 }
